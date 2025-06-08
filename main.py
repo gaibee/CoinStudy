dc8a40e7639e13222dfae4456bba4894652936ca
gaibee <cjh9217@naver.com>
2025-06-08 05:05:51 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, Request
+import uvicorn
+import pyupbit
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # .env 파일을 불러온다.
+
+app = FastAPI()
+
+# 환경변수 또는 직접 입력
+ACCESS_KEY = os.getenv('UPBIT_ACCESS_KEY')
+SECRET_KEY = os.getenv('UPBIT_SECRET_KEY')
+
+upbit = pyupbit.Upbit(ACCESS_KEY, SECRET_KEY)
+
+FEE_RATE = 0.0005 # 업비트 수수료 0.05%
+
+
+@app.get('/test_buy')
+def buy_full_market_order(ticker:str):
+    krw_balance = upbit.get_balance("KRW")
+    if not krw_balance:
+        raise Exception('잔고를 가져올 수 없습니다.')
+        
+    if not krw_balance > 5000: # 최소 매수 금액 고려
+        raise Exception('잔고가 5000원 미만입니다.')
+        
+    krw_to_use = krw_balance * (1 - FEE_RATE)
+    return upbit.buy_market_order(ticker, krw_to_use)
+
+@app.get('/test_sell')
+def sell_full_market_order(ticker:str):
+    btc_balance = upbit.get_balance(ticker)
+    if not btc_balance:
+        raise Exception('BTC 잔고를 가져올 수 없습니다.')
+        
+    return upbit.sell_market_order(ticker, btc_balance)
+
+    
+@app.get('/')
+def index():
+    return '나의 서버에 온걸 환영합니다'
+
+@app.post('/tv_message')
+async def tv_message(request: Request):
+    body = await request.body()
+    message = body.decode("utf-8").strip().lower()
+
+    if message == "buy":
+        try:
+            result = buy_full_market_order("KRW-BTC")
+            return {"status": "buy", "result": result}
+        except Exception as e:
+            return {"status": "failed", "reason": str(e)}
+
+    elif message == "sell":
+        try:
+            result = sell_full_market_order("KRW-BTC")
+            return {"status": "buy", "result": result}
+        except Exception as e:
+            return {"status": "failed", "reason": str(e)}
+
+    else:
+        return {"status": "ignored", "message": message}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=5004, reload=True)
